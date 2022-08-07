5c82d8f2876137fcea7f4588344acd188b0fb503
tangxiaoyu <15932274470@163.com>
2022-08-07 17:41:07 +0800

    .

---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,46 @@
+import { reactive } from "../reactive"
+import { effect } from "../effect"
+describe('effect',()=>{
+    it('happy path', ()=>{
+        const user = reactive({ age: 10});
+        let nextAge;
+        effect(()=>{
+            nextAge = user.age + 1;
+        })
+        expect(nextAge).toBe(11);
+        user.age++;
+        expect(nextAge).toBe(12); 
+    })
+
+    it("return runner",()=>{
+        let foo = 10;
+        const runner = effect(()=>{
+            foo++;
+            return 'foo';
+        })
+        expect(foo).toBe(11);
+        const r = runner();
+        expect(foo).toBe(12);
+        expect(r).toBe("foo");
+    })
+
+    it("scheduler",()=>{
+        let dummy;
+        let run:any;
+        const scheduler = jest.fn(()=>{
+            run = runner;
+        })
+        const obj = reactive({ foo: 1});
+        const runner = effect(()=>{
+            dummy = obj.foo
+        },{ scheduler })
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        expect(dummy).toBe(1);
+        run();
+        expect(dummy).toBe(2);
+
+    })
+})
