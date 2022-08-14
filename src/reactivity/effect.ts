743f8f2b5c3dd5084f77370dc78c885e3b080329
tangxiaoyu <15932274470@163.com>
2022-08-14 17:40:41 +0800

    .

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,17 +1,35 @@
-class ReactiveEffect {
+import { extend } from "../shared";
+
+class ReactiveEffect{
     private _fn: any;
-    constructor(fn,public scheduler?){
+    deps = [];
+    active = true;
+    onStop?: () => void
+    constructor(fn, public scheduler?) {
         this._fn = fn;
     }
     run(){
         activeEffect = this;
         return this._fn();
     }
+    stop(){
+        if(this.active){
+            cleanupEffect(this);
+            if(this.onStop){
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
 }
 
-/**
- * target -> key -> deps
- */
+function cleanupEffect(effect){
+    effect.deps.forEach((dep:any)=>{
+        dep.delete(effect);
+    })
+}
+
+
 const targetMap = new Map();
 export function track(target,key){
     let depsMap = targetMap.get(target);
@@ -24,12 +42,15 @@ export function track(target,key){
         dep = new Set();
         depsMap.set(key,dep);
     }
+
+    if(!activeEffect) return;
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 }
 
-export function trigger(target,key) {
+export function trigger(target,key){
     let depsMap = targetMap.get(target);
-    let dep  = depsMap.get(key);
+    let dep = depsMap.get(key);
     for (const effect of dep) {
         if(effect.scheduler){
             effect.scheduler()
@@ -43,6 +64,15 @@ export function trigger(target,key) {
 let activeEffect;
 export function effect(fn,options:any = {}){
     const _effect = new ReactiveEffect(fn,options.scheduler);
+    Object.assign(_effect,options);
+    extend(_effect,options);
     _effect.run();
-    return _effect.run.bind(_effect);
+    const runner:any = _effect.run.bind(_effect)
+    runner.effect = _effect;
+    return runner
+}
+
+
+export function stop(runner){
+    runner.effect.stop();
 }
